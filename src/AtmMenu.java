2722b3d391a4b9a19214af57c70c2d0118613e43
Gang Ferdinand Dinga <ferdinand.gang4@gmail.com>
2025-08-30 21:42:49 -0500

    Implementing the create account logic

--- src/AtmMenu.java
+++ src/AtmMenu.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AtmMenu extends JFrame {
+public class AtmMenu extends JFrame implements ActionListener {
 //Components on the User Interface
     JTextField userName;
     JTextField userAddress;
     JTextField userId;
     JTextField userPinCode;
+    Border transactionBorder;
+    Border createAccoutBorder;
+
     JPanel panelA;
+    JPanel panelB;
+    JPanel panelC;
 
     //Buttos for user operations
     JButton creatAccountButton;
@@ -25,22 +34,35 @@ public class AtmMenu extends JFrame {
         userId = new JFormattedTextField();
         userPinCode = new JFormattedTextField();
         userName = new JTextField();
+        //userAddress.setVisible(false);
         userAddress = new JTextField();
+        //userAddress.setVisible(false);
         //Buttons
         creatAccountButton = new JButton("Create Accout");
-        checkBanlanceButton = new JButton("AccountBalance");
+        checkBanlanceButton = new JButton("CheckBalance");
         makeDepositButton = new JButton("Make Deposite");
         widrawCashButton = new JButton("Widraw Cash");
 
         //Initializing Radio buttons
         savingAccountRb = new JRadioButton("Saving account");
+        savingAccountRb.setVisible(false);
         currentAccoutRb = new JRadioButton("currentAccount");
-        //adding the text fields to the pannel
+        currentAccoutRb.setVisible(false);
+
+        //Initializing the borders
+        transactionBorder = BorderFactory.createLineBorder(Color.red,15);
+
+        //Initializing the pannels
         panelA = new JPanel();
+        panelB = new JPanel();
+        panelC = new JPanel();
+
         userAddress.setColumns(15);
         userAddress.setText("Your address");
+        userAddress.setVisible(false);
         userName.setColumns(15);
         userName.setText("Your name");
+        userName.setVisible(false);
         userId.setColumns(15);
         userId.setText("Your id");
         userPinCode.setColumns(15);
@@ -48,18 +70,35 @@ public class AtmMenu extends JFrame {
         userPinCode.setColumns(15);
         //Adding text fiels to pannels
         panelA.add(userName);
-        panelA.add(userId);
-        panelA.add(userPinCode);
         panelA.add(userAddress);
-        add(panelA);
-        //addint the buttons
-        add(creatAccountButton);
-        add(checkBanlanceButton);
-        add(widrawCashButton);
-        add(makeDepositButton);
-        //Adding radio buttons to the frame
-        add(savingAccountRb);
-        add(currentAccoutRb);
 
+        //Adding items to pannel B
+        creatAccountButton.addActionListener(this::actionPerformed);
+        panelB.add(creatAccountButton);
+        panelB.add(savingAccountRb);
+        panelB.add(currentAccoutRb);
+        //addint the buttons
+        panelC.add(checkBanlanceButton);
+        panelC.add(makeDepositButton);
+        panelC.add(widrawCashButton);
+
+        //Adding radio buttons to the frame
+
+        add(panelA);
+        add(panelB);
+        add(panelC);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        currentAccoutRb.setVisible(true);
+        savingAccountRb.setVisible(true);
+        userName.setVisible(true);
+        userAddress.setVisible(true);
+        checkBanlanceButton.setVisible(false);
+        widrawCashButton.setVisible(false);
+        makeDepositButton.setVisible(false);
+        creatAccountButton.setText("Click to create account");
     }
 }
